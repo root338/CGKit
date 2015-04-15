06cf56b7e16230389def630173eec895857fb5ac
root338 <1042807191@qq.com>
2015-04-15 22:53:47 +0800

    增加常用宏定义头文件

---
+++ Header/UIViewDefine.h
@@ -0,0 +1,16 @@
+//
+//  UIViewDefine.h
+//  PDPracticeDemo
+//
+//  Created by apple on 15/4/15.
+//  Copyright (c) 2015年 apple. All rights reserved.
+//
+
+#ifndef PDPracticeDemo_UIViewDefine_h
+#define PDPracticeDemo_UIViewDefine_h
+
+#import <UIKit/UIKit.h>
+
+#define __KDeviceIsLandscape UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)
+
+#endif
